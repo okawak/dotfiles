8adf0b6b83883cf5625855c23a64edcfb027713f
okawak <kodak2022@outlook.jp>
2024-05-13 16:51:31 +0900

    add: neovim setting

--- .config/nvim/lua/plugins/ui/barbar.lua
+++ .config/nvim/lua/plugins/ui/barbar.lua
@@ -2,26 +2,26 @@ local map = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
 -- Move to previous/next
-map('n', '<leader>,', '<Cmd>BufferPrevious<CR>', opts)
-map('n', '<leader>.', '<Cmd>BufferNext<CR>', opts)
+map('n', '<Space>,', '<Cmd>BufferPrevious<CR>', opts)
+map('n', '<Space>.', '<Cmd>BufferNext<CR>', opts)
 -- Re-order to previous/next
-map('n', '<leader><', '<Cmd>BufferMovePrevious<CR>', opts)
-map('n', '<leader>>', '<Cmd>BufferMoveNext<CR>', opts)
+map('n', '<Space><', '<Cmd>BufferMovePrevious<CR>', opts)
+map('n', '<Space>>', '<Cmd>BufferMoveNext<CR>', opts)
 -- Goto buffer in position...
-map('n', '<leader>1', '<Cmd>BufferGoto 1<CR>', opts)
-map('n', '<leader>2', '<Cmd>BufferGoto 2<CR>', opts)
-map('n', '<leader>3', '<Cmd>BufferGoto 3<CR>', opts)
-map('n', '<leader>4', '<Cmd>BufferGoto 4<CR>', opts)
-map('n', '<leader>5', '<Cmd>BufferGoto 5<CR>', opts)
-map('n', '<leader>6', '<Cmd>BufferGoto 6<CR>', opts)
-map('n', '<leader>7', '<Cmd>BufferGoto 7<CR>', opts)
-map('n', '<leader>8', '<Cmd>BufferGoto 8<CR>', opts)
-map('n', '<leader>9', '<Cmd>BufferGoto 9<CR>', opts)
-map('n', '<leader>0', '<Cmd>BufferLast<CR>', opts)
+map('n', '<Space>1', '<Cmd>BufferGoto 1<CR>', opts)
+map('n', '<Space>2', '<Cmd>BufferGoto 2<CR>', opts)
+map('n', '<Space>3', '<Cmd>BufferGoto 3<CR>', opts)
+map('n', '<Space>4', '<Cmd>BufferGoto 4<CR>', opts)
+map('n', '<Space>5', '<Cmd>BufferGoto 5<CR>', opts)
+map('n', '<Space>6', '<Cmd>BufferGoto 6<CR>', opts)
+map('n', '<Space>7', '<Cmd>BufferGoto 7<CR>', opts)
+map('n', '<Space>8', '<Cmd>BufferGoto 8<CR>', opts)
+map('n', '<Space>9', '<Cmd>BufferGoto 9<CR>', opts)
+map('n', '<Space>0', '<Cmd>BufferLast<CR>', opts)
 -- Pin/unpin buffer
-map('n', '<leader>p', '<Cmd>BufferPin<CR>', opts)
+map('n', '<Space>p', '<Cmd>BufferPin<CR>', opts)
 -- Close buffer
-map('n', '<leader>c', '<Cmd>BufferClose<CR>', opts)
+map('n', '<Space>c', '<Cmd>BufferClose<CR>', opts)
 -- Wipeout buffer
 --                 :BufferWipeout
 -- Close commands
@@ -31,7 +31,7 @@ map('n', '<leader>c', '<Cmd>BufferClose<CR>', opts)
 --                 :BufferCloseBuffersLeft
 --                 :BufferCloseBuffersRight
 -- Magic buffer-picking mode
-map('n', '<leader>p', '<Cmd>BufferPick<CR>', opts)
+map('n', '<Space>p', '<Cmd>BufferPick<CR>', opts)
 -- Sort automatically by...
 map('n', '<Space>bb', '<Cmd>BufferOrderByBufferNumber<CR>', opts)
 map('n', '<Space>bn', '<Cmd>BufferOrderByName<CR>', opts)
