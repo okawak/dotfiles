d412ee6ec93055e57bee2d44be560cf69875bb7e
okawak <kodak2022@outlook.jp>
2024-05-15 06:09:35 +0900

    mod: wezterm

--- .config/wezterm/format.lua
+++ .config/wezterm/format.lua
@@ -1,39 +1,64 @@
-local wezterm = require 'wezterm';
+local wezterm = require("wezterm")
 
 local function BaseName(s)
-  return string.gsub(s, '(.*[/\\])(.*)', '%2')
+  return string.gsub(s, "(.*[/\\])(.*)", "%2")
 end
 
-wezterm.on(
-  'format-window-title', 
-  function(tab)
-    return BaseName(tab.active_pane.foreground_process_name)
+wezterm.on("format-window-title", function(tab)
+  return BaseName(tab.active_pane.foreground_process_name)
+end)
+
+-- The filled in variant of the < symbol
+local SOLID_LEFT_ARROW = wezterm.nerdfonts.pl_right_hard_divider
+
+-- The filled in variant of the > symbol
+local SOLID_RIGHT_ARROW = wezterm.nerdfonts.pl_left_hard_divider
+
+-- This function returns the suggested title for a tab.
+-- It prefers the title that was set via `tab:set_title()`
+-- or `wezterm cli set-tab-title`, but falls back to the
+-- title of the active pane in that tab.
+function tab_title(tab_info)
+  local title = tab_info.tab_title
+  -- if the tab title is explicitly set, take that
+  if title and #title > 0 then
+    return title
   end
-)
+  -- Otherwise, use the title from the active pane
+  -- in that tab
+  return tab_info.active_pane.title
+end
 
-local HEADER =''
+wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+  local edge_background = "#141621"
+  local background = "#1b1032"
+  local foreground = "#808080"
 
-local SYMBOL_COLOR = { '#ffb2cc', '#a4a4a4' }
-local FONT_COLOR = { '#dddddd', '#888888' }
-local BACK_COLOR = '#2d2d2d'
-local HOVER_COLOR = '#434343'
-
-wezterm.on(
-  'format-tab-title', 
-  function(tab, tabs, panes, config, hover, max_width)
-    local index = tab.is_active and 1 or 2
-
-    local bg = hover and HOVER_COLOR or BACK_COLOR
-    local zoomed = tab.active_pane.is_zoomed and '🔎 ' or ' '
-
-    return {
-      { Foreground = { Color = SYMBOL_COLOR[index] } },
-      { Background = { Color = bg }},
-      { Text = HEADER .. zoomed },
-
-      { Foreground = { Color = FONT_COLOR[index] } },
-      { Background = { Color = bg }},
-      { Text = BaseName(tab.active_pane.title) },
-    }
+  if tab.is_active then
+    background = "#2b2042"
+    foreground = "#c0c0c0"
+  elseif hover then
+    background = "#3b3052"
+    foreground = "#909090"
   end
-)
+
+  local edge_foreground = background
+
+  local title = tab_title(tab)
+
+  -- ensure that the titles fit in the available space,
+  -- and that we have room for the edges.
+  title = wezterm.truncate_right(title, max_width - 2)
+
+  return {
+    { Background = { Color = edge_background } },
+    { Foreground = { Color = edge_foreground } },
+    { Text = SOLID_LEFT_ARROW },
+    { Background = { Color = background } },
+    { Foreground = { Color = foreground } },
+    { Text = title },
+    { Background = { Color = edge_background } },
+    { Foreground = { Color = edge_foreground } },
+    { Text = SOLID_RIGHT_ARROW },
+  }
+end)
--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -17,22 +17,25 @@ config.key_tables = require("keybinds").key_tables
 config.leader = { key = ",", mods = "CTRL", timeout_milliseconds = 2000 }
 
 -- colors
-config.color_scheme = "nord"
-config.window_background_opacity = 0.93
+config.color_scheme = "Tokyo Night Storm"
 
 -- font
-config.font = require("wezterm").font("Hack Nerd Font")
-config.font_size = 13.0
+config.font = require("wezterm").font("Firge35Nerd Console")
+config.font_size = 15.0
 config.window_frame = {
-  font = wezterm.font({ family = "Roboto", weight = "Bold" }),
-  font_size = 11.0,
+  font = wezterm.font({ family = "Firge35Nerd Console", weight = "Bold" }),
+  font_size = 13.0,
 }
 
--- status
-config.status_update_interval = 1000
+-- display
+config.window_background_opacity = 0.90
 
--- window decorations
--- config.window_decorations = "RESIZE"
+-- startup with full-screen mode
+local mux = wezterm.mux
+wezterm.on("gui-startup", function(cmd)
+  local tab, pane, window = mux.spawn_window(cmd or {})
+  window:gui_window():toggle_fullscreen()
+end)
 
 -- mouse binds
 config.mouse_bindings = require("mousebinds").mouse_bindings
