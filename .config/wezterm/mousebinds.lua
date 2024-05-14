627890df8247f9874a8304434e83060694050eaf
okawak <kodak2022@outlook.jp>
2024-05-15 02:03:25 +0900

    add: wezterm

---
+++ .config/wezterm/mousebinds.lua
@@ -0,0 +1,21 @@
+local wezterm = require 'wezterm'
+local act = wezterm.action
+local config = {}
+
+config.mouse_bindings = {
+  -- Scrolling up while holding CTRL increases the font size
+  {
+    event = { Down = { streak = 1, button = { WheelUp = 1 } } },
+    mods = 'CTRL',
+    action = act.IncreaseFontSize,
+  },
+
+  -- Scrolling down while holding CTRL decreases the font size
+  {
+    event = { Down = { streak = 1, button = { WheelDown = 1 } } },
+    mods = 'CTRL',
+    action = act.DecreaseFontSize,
+  },
+}
+
+return config
