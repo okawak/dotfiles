ea47cf1131f08e64c0ae490cc8ac4a9ccefdde56
okawak <kodak2022@outlook.jp>
2024-05-16 21:19:57 +0900

    mod: lsp setting

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -19,16 +19,41 @@ vim.api.nvim_create_autocmd("LspAttach", {
     set("n", "]d", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>", { buffer = true })
     set("n", "<space>q", "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>", { buffer = true })
     set("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", { buffer = true })
+
+    -- automatically format
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      pattern = { "*.rs" }, -- only for rust-analyzer
+      callback = function()
+        vim.lsp.buf.format({
+          buffer = ctx.buf,
+          filter = function(f_client)
+            return f_client.name ~= "null-ls"
+          end,
+          async = false,
+        })
+      end,
+    })
   end,
 })
 
 require("mason").setup()
-require("mason-lspconfig").setup()
-require("mason-lspconfig").setup_handlers {
+require("mason-lspconfig").setup({
+  ensure_installed = {
+    "black",
+    "cbfmt",
+    "clang-format",
+    "clangd",
+    "lua-ls",
+    "markdownlint",
+    "shfmt",
+    "stylua",
+  },
+})
+require("mason-lspconfig").setup_handlers({
   function(server_name)
-    require("lspconfig")[server_name].setup {}
+    require("lspconfig")[server_name].setup({})
   end,
-}
+})
 
 -- lsp handler setting
 capabilities = require("cmp_nvim_lsp").default_capabilities()
@@ -36,7 +61,7 @@ capabilities = require("cmp_nvim_lsp").default_capabilities()
 -- add after lsp setting
 vim.opt.completeopt = "menu,menuone,noselect"
 
-local cmp = require"cmp"
+local cmp = require("cmp")
 cmp.setup({
   snippet = {
     expand = function(args)
@@ -57,5 +82,5 @@ cmp.setup({
     { name = "luasnip" },
   }, {
     { name = "buffer" },
-  })
+  }),
 })
--- .config/nvim/lua/plugins/lsp/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/lsp/nvim-lspconfig.lua
@@ -1,3 +1,15 @@
 return {
-  "neovim/nvim-lspconfig"
+  "neovim/nvim-lspconfig",
+  config = function()
+    local lspconfig = require("lspconfig")
+    lspconfig.rust_analyzer.setup({
+      settings = {
+        ["rust-analyzer"] = {
+          check = {
+            command = "clippy",
+          },
+        },
+      },
+    })
+  end,
 }
