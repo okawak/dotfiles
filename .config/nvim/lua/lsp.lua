607ed4dfeadda137d5d0eb9102e26793300d4cd2
okawak <kodak2022@outlook.jp>
2025-09-13 11:36:08 +0900

    status: 2025_09_13

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -44,11 +44,11 @@ require("mason-lspconfig").setup({
     "tsserver",
   },
 })
-require("mason-lspconfig").setup_handlers({
-  function(server_name)
-    require("lspconfig")[server_name].setup({})
-  end,
-})
+-- require("mason-lspconfig").setup_handlers({
+--   function(server_name)
+--     require("lspconfig")[server_name].setup({})
+--   end,
+-- })
 
 -- lsp handler setting
 capabilities = require("cmp_nvim_lsp").default_capabilities()
